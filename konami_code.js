40d797cde59f4a4b5ed027410beda7f25133b5af
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-03 19:37:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -23,7 +23,7 @@ function init() {
 }*/
 
 const body = document.getElementsByTagName("BODY")[0]
-From Rochel Levi to Everyone:  03:37 PM
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   let index = 0;
