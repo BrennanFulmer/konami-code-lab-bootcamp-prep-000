c3b5a76a0d2cb9b7b9f8616f7f0155189b4afe85
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-03 19:42:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,3 @@
-/*const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-
-
-function init() {
-  let step = 0;
-  
-  const body = document.getElementsByTagName("BODY")[0];
-
-  body.addEventListener("keydown", press => {
-    const button = parseInt(press.which);
-
-    if (button == code[step]) {
-      step++;
-      if (step == 10) {
-        window.alert("Konami Code entered!");
-        step = 0;
-      }
-    } else {
-      step = 0;
-    }
-  });
-}*/
-
 const body = document.getElementsByTagName("BODY")[0]
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
